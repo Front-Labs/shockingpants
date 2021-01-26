1abe16adcc846df6e4fe1714fa9a0d8896d5e8f3
Daan Roelofs <daanroelofs599@gmail.com>
2021-01-26 18:08:19 +0100

    Toggle menu

--- src/index.js
+++ src/index.js
@@ -3,20 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import SliderJS from './SliderJS.js';
 import Featured from './Featured.js';
+import Toggle from './Toggle';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Featured/ >
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-{/* <div style={{position: "relative"}}>
-  <SliderJS />
-</div> */}
+ReactDOM.render(<Toggle />, document.getElementById('root'));
+  
