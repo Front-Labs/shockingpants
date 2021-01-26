3e7ec1eaf7b04a5cb1537c6ebee59b05fb249222
Chris Kirk <uberengineer@gmail.com>
2021-01-26 18:20:59 +0100

    merging branches

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Toggle from './Toggle'
 import VideoSlider from './VideoSlider';
 import { SliderData } from './SliderData';
 
 function App() {
-    return <VideoSlider slides={SliderData} />
+    return (
+        <>
+            <VideoSlider slides={SliderData} />
+            <Toggle />
+        </>
+    )
 }
 
 export default App;
