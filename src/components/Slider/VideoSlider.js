56354f7f31fdc3e484a680e3781da7d5c87d4fb4
Chris Kirk <uberengineer@gmail.com>
2021-02-16 15:23:53 +0100

    added about section

--- src/components/Slider/VideoSlider.js
+++ src/components/Slider/VideoSlider.js
@@ -1,36 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Vimeo from '@u-wave/react-vimeo';
-import {SliderData} from './SliderData';
+import { SliderData } from './SliderData';
 import './VideoSlider.css'
 
-const VideoSlider = ({slides}) => {
-    const [current, setCurrent] = useState(0);
-    const length = slides.length;
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrent(current === length-1 ? 0 : current + 1);
-            //Featured Slide Transition
-        }, 10000);
-        return () => clearInterval(interval);
-    });
-    return (
-        <section className='slider'>
-            {SliderData.map((slide, index) => {
-                return (
-                    <div
-                        className={index === current ? 'slide active' : 'slide'}
-                        key = {index}
-                    >
-                        {index === current && (
-                            <iframe title="1" src={slide.url} min-width="100%" max-height="100%">
-                            </iframe>
-                        )}
-                    </div>
-                )
-            })}
-        </section>
-    );
+const VideoSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent(current === length - 1 ? 0 : current + 1);
+      //Featured Slide Transition
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+  return (
+    <section className='slider'>
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <Vimeo
+                video={slide.videoId}
+                responsive='true'
+                background='true'
+                quality='auto'
+              />
+            )}
+          </div>
+        )
+      })}
+    </section>
+  );
 };
 
 export default VideoSlider;
-
