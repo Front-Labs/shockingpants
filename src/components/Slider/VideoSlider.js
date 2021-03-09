f58fc82444af8d12eb65b5e3d9e03d778c42ec68
Chris Kirk <uberengineer@gmail.com>
2021-03-09 17:58:57 +0100

    quality and speed improvements

--- src/components/Slider/VideoSlider.js
+++ src/components/Slider/VideoSlider.js
@@ -1,41 +1,47 @@
-import React, { useState, useEffect } from "react";
-import Vimeo from '@u-wave/react-vimeo';
-import { sliderData } from './sliderData';
-import CardProgress from './Progress/CardProgress'
+import React, { useState, useEffect } from "react"
+import { sliderData } from './sliderData'
+// import CardProgress from './Progress/CardProgress'
 import './VideoSlider.css'
 import Card from './Cards/Card'
-import VimeoPlayer from './VimeoPlayer'
+import Player from '@vimeo/player'
 
 const VideoSlider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
+  const [player, setPlayer] = useState(null)
+  const [current, setCurrent] = useState(0)
+
+  const length = slides.length
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrent(current === length - 1 ? 0 : current + 1);
+      setCurrent(current === length - 1 ? 0 : current + 1)
       //Featured Slide Transition
     }, 10000);
-    return () => clearInterval(interval);
-  });
-  return (
+    return () => clearInterval(interval)
+  })
 
+  useEffect(() => {
+    setPlayer(new Player('player', {
+      id: slides[0].videoId,
+      quality: "360p",
+      background: true,
+      responsive: true
+    })) 
+  }, [])
+
+  useEffect(() => {
+    if (player) {
+      player.loadVideo(slides[current].videoId)
+    }
+  }, [current])
+
+  return (
     <section id='slider'>
+
       <div className="logo-mark">
         <img src="images/Post-Mark.png" alt="logo" />
       </div>
 
-      {sliderData.map((slide, index) => {
-        return (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-                <div id="{slide.videoId}"></div>
-            )
-            }
-          </div>
-        )
-      })}
+    <div id="player"></div>
+
       <div className="cards">
         {sliderData.map((slide, index) => {
           return (
@@ -47,9 +53,8 @@ const VideoSlider = ({ slides }) => {
           )
         })}
       </div>
-    </section >
-  );
-};
+    </section>
+  )
+}
 
-export default VideoSlider;
-
+export default VideoSlider
