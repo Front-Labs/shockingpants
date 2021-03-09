e04b287daa5254464feb8936df58fa8b11e264b1
Daan Roelofs <daanroelofs599@gmail.com>
2021-03-09 18:00:06 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { Route, BrowserRouter } from 'react-router-dom'
 import EmbeddedPlayer from './components/Portfolio/Embeddedplayer/Embeddedplayer';
@@ -28,5 +27,3 @@ ReactDOM.render(routs, document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-reportWebVitals();
