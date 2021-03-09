cce0e41ce2f1b528611785e98d4261a8c4d87608
Daan Roelofs <daanroelofs599@gmail.com>
2021-03-09 14:46:30 +0100

    routing embedded player

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import VideoSlider from './components/Slider/VideoSlider';
 import Portfolio from './components/Portfolio/Portfolio';
 import { sliderData } from './components/Slider/sliderData';
 import Contact from './components/Contact/Contact';
-// import EmbeddedPlayer from './components/Embeddedplayer/Embeddedplayer';
 
 function App() {
   return (
--- src/components/Embeddedplayer/Embeddedplayer.js
+++ src/components/Embeddedplayer/Embeddedplayer.js
@@ -4,6 +4,11 @@ import './Embeddedplayer.css'
 const EmbeddedPlayer = () => {
     return (
       <section id="embeddedplayer-container">
+       <a href="http://localhost:3000/#portfolio1">
+          <div className="times">
+            <img src="images/times-circle-regular.svg" alt="times" />
+          </div>
+        </a>
         <ul className="player">
           <li className="video-link"> 
             <iframe title="vimeo-player" src="https://player.vimeo.com/video/511153244" width="854" height="480" frameborder="0" allowfullscreen></iframe>
