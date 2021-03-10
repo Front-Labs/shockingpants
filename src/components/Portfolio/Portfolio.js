569f1190cfc28f863cffba68ba7ee47a8dc17ffb
Daan Roelofs <daanroelofs599@gmail.com>
2021-03-10 16:58:51 +0100

    props

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,78 +1,51 @@
 import React, { useState } from 'react';
 import './Portfolio.css';
-import Vimeo from '@u-wave/react-vimeo';
-import { Link } from 'react-router-dom'
+// import Vimeo from '@u-wave/react-vimeo';
+import { Link } from 'react-router-dom';
+import { portfolioData } from './portfolioData.js';
+import Player from '@vimeo/player';
 
-function Portfolio() {
+function Portfolio({ portfolio }) {
+const [player, setPlayer] = useState(null)
 
-    return (
+  return (
     <section id="portfolio1" className="portfolio-page">
-      <div className="title">
-        <Vimeo
-          video='511153244'
-          responsive='true'
-          muted='true'
-          quality='auto'
-          controls='false'
-        />
-        <Link to="/embeddedplayer" className="glitch">
-          <span aria-hidden="true">
-            You don't own me
-          </span>
-            You don't own me
-          <span aria-hidden="true">
-            You don't own me
-          </span>
-       </Link>
-      </div>
-      <div className="title">
-        <Vimeo
-          video='511146982'
-          responsive='true'
-          muted='true'
-          quality='auto'
-          controls='false'
-        /><a href="https://vimeo.com/511146982" target="_blank" rel="noopener noreferrer" className="glitch">
-          <span aria-hidden="true">Huawei: Rethink possibilities</span>
-                        Huawei: Rethink possibilities<span aria-hidden="true">Huawei: Rethink possibilities</span>
-        </a>
-      </div>
-      <div className="title">
-        <Vimeo
-          video='511150620'
-          responsive='true'
-          muted='true'
-          quality='auto'
-          controls='false'
-        /><a href="https://vimeo.com/511150620" target="_blank" rel="noopener noreferrer" className="glitch">
-          <span aria-hidden="true">ABN AMRO: The dark side of Money</span>
-                        ABN AMRO: The dark side of Money<span aria-hidden="true">ABN AMRO: The dark side of Money</span>
-        </a>
-      </div>
-      <div className="title">
-        <Vimeo
-          video='511149009'
-          responsive='true'
-          muted='true'
-          quality='auto'
-          controls='false'
-        /><a href="https://vimeo.com/511149009" target="_blank" rel="noopener noreferrer" className="glitch">
-          <span aria-hidden="true">Mazda: MX-30 Designer Review</span>
-                        Mazda: MX-30 Designer Review<span aria-hidden="true">Mazda: MX-30 Designer Review</span>
-        </a>
-      </div>
-      <div className="title">
-        <Vimeo
-          video='511149224'
-          responsive='true'
-          muted='true'
-          quality='auto'
-          controls='false'
-        /><a href="https://vimeo.com/511149224" target="_blank" rel="noopener noreferrer" className="glitch">
-          <span aria-hidden="true">Foot Locker // Di-Meh</span>
-                        Foot Locker // Di-Meh<span aria-hidden="true">Foot Locker // Di-Meh</span>
-        </a>
-      </div>
+
+
+      {portfolioData.map((tab, index) => {
+          
+        // let iframe = document.querySelector('title');
+        // let player = new Vimeo.Player(iframe);
+        // player.getVideoTitle().then(function (title) {
+        //   console.log('title:', title);
+        // });
+        // setPlayer(new Player( index, {
+        //   id: tab.url,
+        //   quality: "360p",
+        //   responsive: true,
+        //   muted: true,
+        //   dnt: true,
+        //   controls: false
+        // }))
+        return (
+          <div id={ index } className="title">
+        
+
+            <Link to="/embeddedplayer" className="glitch">
+              <span aria-hidden="true">
+                You don't own me
+                </span>
+                  You don't own me
+                <span aria-hidden="true">
+                You don't own me
+                </span>
+            </Link>
+            
+          </div>
+
+        )
+      })}
+
     </section>
   );
 }
