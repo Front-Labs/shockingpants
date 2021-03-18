d1f6a28134391d6c24cf7ec6b8962d1af92a59ba
Chris Kirk <uberengineer@gmail.com>
2021-03-18 18:53:34 +0100

    Featured API working

--- Frontend/src/components/Featured/VideoSlider.js
+++ Frontend/src/components/Featured/VideoSlider.js
@@ -7,54 +7,47 @@ import Player from '@vimeo/player'
 export default function VideoSlider() {
   const [player, setPlayer] = useState(null)
   const [current, setCurrent] = useState(0)
-  const [featuredData, setFeaturedData] = useState([])
-  
+  const [featureData, setFeatureData] = useState([])
+
   useEffect(() => {
     fetch('http://localhost:3001/featured').then(response => response.json())
-      .then(featuredData => {
-        setFeaturedData(featuredData)
+      .then(featureData => {
+        setFeatureData(featureData)
+        console.log("getting")
+        setPlayer(new Player('player', {
+          id: featureData[0].uri.replace('/videos/', ''),
+          quality: "360p",
+          background: true,
+          responsive: true
+        }))
       }).catch(error => {
         console.log(error.message)
       })
   }, [])
 
-  const length = featuredData.length
-
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrent(current === length - 1 ? 0 : current + 1)
-    }, 10000);
+      setCurrent(current === featureData.length - 1 ? 0 : current + 1)
+    }, 5000);
     return () => clearInterval(interval)
   })
 
-  useEffect((featuredData) => {
-    let videoId = featuredData[0].uri.replace('/videos/', '')
-    setPlayer(new Player('player', {
-      id: videoId,
-      quality: "360p",
-      background: true,
-      responsive: true
-    }))
-  }, [])
-
-  useEffect((featuredData, player, current) => {
-    let videoId = featuredData[current].uri.replace('/videos/', '')
+  useEffect(() => {
     if (player) {
-      player.loadVideo(videoId)
+      player.loadVideo(featureData[current].uri.replace('/videos/', ''))
     }
-  }, [featuredData, player, current])
+  }, [current])
 
   return (
     <section id='slider'>
-
       <div className="logo-mark">
         <img src="images/Post-Mark.png" alt="logo" />
       </div>
 
-    <div id="player"></div>
+      <div id="player"></div>
 
       <div className="cards">
-        {featuredData.map((slide, index) => {
+        {featureData.map((slide, index) => {
           return (
             <Card
               title={slide.name}
