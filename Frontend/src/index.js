df66b23eb2091c5a731e80efc2c4d7af33eb1d5f
Chris Kirk <uberengineer@gmail.com>
2021-03-22 13:21:44 +0100

    removing node_modules

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -15,14 +15,6 @@ const routs = (
 );
 
 ReactDOM.render(routs, document.getElementById('root'))
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App/ >
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
