cbb080abcc653a4d5bf32fa7525b46c425d26cce
Chris Kirk <uberengineer@gmail.com>
2021-03-30 15:13:13 +0200

    secure token

--- Server.js
+++ Server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require("express");
 const cors = require("cors");
 
@@ -8,9 +10,9 @@ app.use(cors());
 
 let Vimeo = require('vimeo').Vimeo;
   let client = new Vimeo(
-    "24d7467f36afb8d8e9ce46b73d2b8c78595bb104",
-    "QrLYalEjnvrwB25dBIXTLEdi5YG/XaqNcgv5TwFqpQl9zh8rOV/ajevqH2DxjQoAYa0YflRbB+x+3atpOV9l2GDVq+C9Tuncn+oH9wdig4d0c7FxiVGdadoIE96aIrlR",
-    "1efc32639d8c4ab5996a9c3cad5ad1d4",
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.ACCESS_TOKEN
   );
 
 //Featured Endpoint
