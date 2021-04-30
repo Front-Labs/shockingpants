8ea13d4e4ce0a8d36e21e45a426263d24db6f70b
Chris Kirk <uberengineer@gmail.com>
2021-04-30 14:20:25 +0200

    contact page scrolling for quotes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,24 +7,6 @@ const Contact = () => {
       backgroundImage: "url(images/contact2.jpg)"
     }}>
 
-      <ul className="contact-buttons">
-        <li>
-          <a href="https://vimeo.com/postmark" target="_blank" rel="noreferrer">
-            <img src="images/vimeo.svg" alt="vimeo" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/mark-schokkenbroek-78169541/" target="_blank" rel="noreferrer">
-            <img src="images/linkedin.svg" alt="linkedin" />
-          </a>
-        </li>
-        <li>
-          <a href="https://goo.gl/maps/ho1xhrgE9cPNo5Ei8" target="_blank" rel="noreferrer">
-            <img src="images/map-marker.svg" alt="map-marker" />
-          </a>
-        </li>
-      </ul>
-
       <div className="quote top">
         <h3>'Mark is a skilled pitch editor'</h3>
       </div>
@@ -33,6 +15,26 @@ const Contact = () => {
       </div>
       <div className="quote low">
         <h3>'Editor with an eye for detail'</h3>
+      </div>
+
+      <div className="contact-buttons">
+        <ul>
+          <li>
+            <a href="https://vimeo.com/postmark" target="_blank" rel="noreferrer">
+              <img src="images/vimeo.svg" alt="vimeo" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/mark-schokkenbroek-78169541/" target="_blank" rel="noreferrer">
+              <img src="images/linkedin.svg" alt="linkedin" />
+            </a>
+          </li>
+          <li>
+            <a href="https://goo.gl/maps/ho1xhrgE9cPNo5Ei8" target="_blank" rel="noreferrer">
+              <img src="images/map-marker.svg" alt="map-marker" />
+            </a>
+          </li>
+        </ul>
       </div>
 
       <a href="#featured">
