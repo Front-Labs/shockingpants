5aef781342e7d32290369f1c47d09c286a2e24a4
Chris Kirk <uberengineer@gmail.com>
2021-05-03 15:36:27 +0200

    Concept for contact scrolling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,4 @@
+import Observer from './Observer'
 import './contact.scss'
 
 const Contact = () => {
@@ -23,33 +24,26 @@ const Contact = () => {
           />
         </div>
       </div>
-      <div className="quote top">
-        <h3>'Mark is a skilled pitch editor'</h3>
-      </div>
-      <div className="quote mid">
-        <h3>'I'm a real family man'</h3>
-      </div>
-      <div className="quote low">
-        <h3>'Editor with an eye for detail'</h3>
-      </div>
-      <div className="contact-buttons">
-        <ul>
-          <li>
-            <a href="https://vimeo.com/postmark" target="_blank" rel="noreferrer">
-              <img src="images/vimeo.svg" alt="vimeo" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/mark-schokkenbroek-78169541/" target="_blank" rel="noreferrer">
-              <img src="images/linkedin.svg" alt="linkedin" />
-            </a>
-          </li>
-          <li>
-            <a href="https://goo.gl/maps/ho1xhrgE9cPNo5Ei8" target="_blank" rel="noreferrer">
-              <img src="images/map-marker.svg" alt="map-marker" />
-            </a>
-          </li>
-        </ul>
+      <div className="contact-container">
+        <div className="contact-buttons">
+          <ul>
+            <li>
+              <a href="https://vimeo.com/postmark" target="_blank" rel="noreferrer">
+                <img src="images/vimeo.svg" alt="vimeo" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/mark-schokkenbroek-78169541/" target="_blank" rel="noreferrer">
+                <img src="images/linkedin.svg" alt="linkedin" />
+              </a>
+            </li>
+            <li>
+              <a href="https://goo.gl/maps/ho1xhrgE9cPNo5Ei8" target="_blank" rel="noreferrer">
+                <img src="images/map-marker.svg" alt="map-marker" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <a href="#featured">
         <div className="arrow">
@@ -58,7 +52,22 @@ const Contact = () => {
           <span></span>
         </div>
       </a>
-    </section>
+      <Observer
+        position="quote top"
+        quote="Mark is a skilled pitch editor"
+        description="Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum"
+      />
+      <Observer
+        position="quote mid"
+        quote="I'm a real family man"
+        description="Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum"
+      />
+      <Observer
+        position="quote low"
+        quote="Editor with an eye for detail"
+        description="Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum"
+      />
+    </section >
   )
 }
 
