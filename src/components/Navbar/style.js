ade2889773fa51d06f554afd436674a782763034
Daan Roelofs <daanroelofs599@gmail.com>
2021-05-04 17:41:57 +0200

    mobile reponsive pages

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -27,29 +27,24 @@ export const StyledBurger = styled.button`
   border: none;
   cursor: pointer;
   padding: 0;
+  z-index: 2;
 
   &:focus {
     outline: none;
   }
 
-  @media screen and (max-width: 2000px) {
-    position: absolute;
-    top: 2.5rem;
-    right: 3rem;
-  }
-
-  @media screen and (max-width: 1024px) {
-    position: absolute;
-    top: 2.5rem;
-    right: 3rem;
-  }
-
   @media screen and (max-width: 400px) {
     position: fixed;
-    z-index: 2;
     top: 2rem;
     right: 2rem;
   }
+
+  @media screen and (min-width: 1024px) {
+    position: absolute;
+    top: 2.5rem;
+    right: 3rem;
+  }
+
 
   div {
     background: white;
@@ -71,77 +66,59 @@ export const StyledBurger = styled.button`
       transform: ${({ open }) => open ? 'rotate(-50deg)' : 'rotate(0)'};
     }
 
-    @media screen and (max-width: 2000px) {
-      width: 2rem;
+    @media screen and (max-width: 400px) {
+      width: 1.9rem;
       height: 0.25rem;
     }
 
-    @media screen and (max-width: 1024px) {
+    @media screen and (min-width: 1024px) {
       width: 2.5rem;
-      height: 0.35rem;
-    }
-
-    @media screen and (max-width: 400px) {
-      width: 1.9rem;
-      height: 0.35rem;
-    }
+      height: 0.25rem;
+    }    
   }
 `;
 
 export const StyledMenu = styled.nav`
-  position: absolute;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   background: transparent;
-  height: 100vh;
   padding: 4rem;
-  top: 0;
   right: 0;
   transition: transform 0.3s ease-in-out;
-  // transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+  z-index: 1;
 
-  @media screen and (max-width: 2000px) {
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-  }
-
-  @media screen and (max-width: 1024px) {
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-    height: 50vh;
-    width: 100%;
-    align-items: center;
-  }
-  
   @media screen and (max-width: 400px) {
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-    height: 50vh;
-    width: 100%;
-    z-index: 2;
     position: fixed;
+    width: 100%;
+    top: 1.5rem;
   }
+
+  @media screen and (min-width: 1024px) {
+    position: absolute;
+    align-items: flex-start;
+    top: 5rem;
+  }
+
 
   a {
-    font-size: 2rem;
     text-transform: uppercase;
-    padding: 2rem;
     font-weight: 500;
     letter-spacing: 0.5rem;
     color: white;
     opacity: 0.8;
-    list-style: none;
     text-decoration: none;
     transition: color 0.3s linear;
-
-    @media screen and (max-width: 1024px) {
-      font-size: 1.5rem;
-      text-align: center;
-      padding: 1.3rem;
-    }
+    text-align: center;
 
     @media screen and (max-width: 400px) {
       font-size: 1rem;
-      text-align: center;
       padding: 1.3rem;
+    }
+
+    @media screen and (min-width: 1024px) {
+      font-size: 2rem;
+      padding: 1.5rem;
     }
 
     &:hover {
