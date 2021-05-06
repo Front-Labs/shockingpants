65481ead10e5ae957ee26dab149ea81d9ae828b5
Chris Kirk <uberengineer@gmail.com>
2021-05-06 16:41:23 +0200

    added https to endpoints

--- Server.js
+++ Server.js
@@ -42,5 +42,5 @@ app.get("/portfolio", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+  console.log(`Server running at https://localhost:${port}`)
 });
