d2fd9b16ba1ee1df858654fe44fb76a9a6b525bf
Chris Kirk <uberengineer@gmail.com>
2021-05-18 21:42:18 +0200

    observer responsive logic

--- src/components/Contact/Observer.js
+++ src/components/Contact/Observer.js
@@ -1,21 +1,25 @@
 import { useEffect, useState, useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Observer({ position, quote, description }) {
   const [isVisible, setVisible] = useState(false);
   const domRef = useRef();
+  const isMobilePortrait = useMediaQuery({ maxWidth: 1280, orientation: "portrait" });
   useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
-  }, []);
-  return (
-    <div
-      ref={domRef}
-      className={`fade-in-section ${position} ${isVisible ? 'is-visible' : ''}`}
-    >
-      <h3>{quote}</h3>
-      <span>{description}</span>
-    </div>
-  );
+    if (!isMobilePortrait) {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+    }
+    }, );
+return (
+  <div
+    ref={domRef}
+    className={`fade-in-section ${position} ${isVisible ? 'is-visible' : ''}`}
+  >
+    <h3>{quote}</h3>
+    <span>{description}</span>
+  </div>
+);
 }
